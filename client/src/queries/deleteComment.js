29aafaa7cd0caea66c7ff2034c5c1cb625aa66d4
Daniel Soares <danielandradesoares@hotmail.com>
2022-03-23 12:40:55 -0300

    delete comment mutation

---
+++ client/src/queries/deleteComment.js
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+const DELETE_COMMENT = gql`
+  mutation DeleteComment($postId: ID!, $commentId: ID!) {
+    deleteComment(postId: $postId, commentId: $commentId) {
+      id
+      comments {
+        id
+        userName
+        createdAt
+        body
+      }
+      commentCount
+    }
+  }
+`;
+
+export default DELETE_COMMENT;
